f796bf2967f40c43065400d379a7b3eb29eb9166
Your Name <you@example.com>
2025-03-10 15:18:23 +0600

    allmost close seasion

--- module-33/jsonPlaceHolder.js
+++ module-33/jsonPlaceHolder.js
@@ -4,9 +4,20 @@ const loadData=()=>{
     .then(data=>{
         console.log(data);
         
-    })
+    });
+    
 }
 function petsData(pets){
     window.location.href="./pet.html"
     }
    
+
+const data = async () => {
+    try {
+        const response = await fetch("https://openapi.programming-hero.com/api/peddy/pets");
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
